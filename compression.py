1ac70775cbd121ef821e0455e7a4844c807ebf28
Graham Keenan <grahamkeenan@hotmail.co.uk>
2018-08-21 20:55:04 +0100

    THIS WILL WORK

--- compression.py
+++ compression.py
@@ -1,14 +1,23 @@
 import sys
 import pickle
 import itertools
+import base64
+import bz2
 import utilities as utils
-
+from collections import Counter
 
 
 if __name__ == "__main__":
     f_in = utils.big_read(sys.argv[1])
     tmp = [elem for elem in itertools.chain(*f_in)]
 
-    out, codec1 = utils.huffman_encode(f_in)
-    out2, codec2 = utils.huffman_encode(tmp, freqs=True)
+    compressed = bz2.compress(bytes(tmp))
+
+    # Small output, large codec
+    # out1, codec1 = utils.huffman_encode(compressed)
+    # Large output, small codec
+    # out2, codec2 = utils.huffman_encode(compressed, freqs=True)
     
+
+    # Large codec decrypts large output
+    # Small codec does not decrypt small output1
