268ec0f88691a8f2ef17abebc2150b5975b56657
Graham Keenan <grahamkeenan@hotmail.co.uk>
2018-08-28 17:15:54 +0100

    Autocommit

--- compression.py
+++ compression.py
@@ -1,35 +1,15 @@
 import sys
-import pickle
-import itertools
-import base64
 import zlib
-import numpy as np
 import math
+import pickle
+import base64
+import itertools
+import numpy as np
 import utilities as utils
 
-def bit_totaller(data, bit=0):
-    print(f"Current bit: {bit}")
-    bit_string = ""
-    for elem in data:
-        bits = "{:08b}".format(elem)
-        bit_string += bits[bit]
-    
-    try:
-        logarithm = math.log10(int(bit_string, 2))
-        return logarithm
-    except Exception:
-        print("Most likely all zeros, returning total number of zeros")
-        return len(str(bit_string))
-
-
 if __name__ == "__main__":
-    f_in = [list(elem) for elem in utils.big_read(sys.argv[1], chunk_size=16384)]
+    f_in = [list(elem) for elem in utils.big_read(sys.argv[1])]
     tmp = [elem for elem in itertools.chain(*f_in)]
 
-    
-    out = []
-    for i in range(8):
-        x = bit_totaller(tmp, bit=i)
-        out.append(bytes(str(x).encode("utf-8")).hex())
-
-    print(out)
+    x = math.log2(tmp[1])
+    print(x)
